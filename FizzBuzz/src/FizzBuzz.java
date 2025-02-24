115ce7b28b933f401e54273b89c94a454accc9b1
Ricsi <shadowparty69@gmail.com>
2025-02-24 12:19:27 +0100

    homework

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -2,19 +2,26 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("Please write a number which must be greater than 0 ");
-        Scanner scanner = new Scanner(System.in);
-        String number = scanner.nextLine();
-        int i = scanner.nextInt();
-        if (i <= 105 && i % 3 == 0 && i % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (i <= 105 && i % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (i <= 105 && i % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.err.println("Error!");
+        Scanner sc = new Scanner(System.in);
+        int i;
+        do {
+            System.out.println("Please write a number which must be greater than 0! ");
+            i = sc.nextInt();
+            if (i < 1) {
+                System.err.println("The number must be greater than 0!");
+            }
+        } while (i < 0);
+        for (int j = 1; j < i + 1; j++) {
+            if (j % 3 == 0 && j % 5 == 0) {
+                System.out.println("FizzBuzz!");
+            } else if (j % 3 == 0) {
+                System.out.println("Fizz!");
+            } else if (j % 5 == 0) {
+                System.out.println("Buzz!");
+            } else {
+                System.out.println(j);
+            }
         }
-        scanner.close();
+        sc.close();
     }
 }
