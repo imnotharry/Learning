a43c1f7081426bf475a2b348d2141350a9b1aad3
Ricsi <shadowparty69@gmail.com>
2025-04-08 00:52:36 +0200

    MethodsPartTwo

--- methodsparttwo/src/MethodsPartTwo.java
+++ methodsparttwo/src/MethodsPartTwo.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MethodsPartTwo {
     public static void main(String[] args) {
         System.out.println("Task 1.");
@@ -15,7 +17,7 @@ public class MethodsPartTwo {
         System.out.println(writeEmptyString(array1));
         System.out.println();
         System.out.println("Task 5.");
-        System.out.println(get428Percent(100));
+        System.out.println(get42Point8Percent(100));
         System.out.println();
         System.out.println("Task 6.");
         System.out.println("The doubled length value of the given sentence is: " + writeDoubleLength("I will, but not today."));
@@ -35,46 +37,71 @@ public class MethodsPartTwo {
         System.out.println("If i divide with 1, we will get: " + printDividedValueBy100(1));
         System.out.println();
         System.out.println("Task 11.");
-
-
-
+        System.out.println("This is a prime number, so it'll return the square of it: " + printWeirdPrime(11));
+        System.out.println("This is not a prime but it's divisible by 4, so it'll return the double of the value: " + printWeirdPrime(12));
+        System.out.println("This is not a prime and not divisible by 4, so it'll return the triple of the value: " + printWeirdPrime(9));
+        System.out.println();
+        System.out.println("Task 12.");
+        array1 = new String[]{"clutch", "brake", "gas"};
+        System.out.println("The length of these words are: " + Arrays.toString(printStringLengths(array1)));
+        System.out.println();
+        System.out.println("Task 13.");
+        int averageSalary = 100000;
+        System.out.println("The total salary you earned is:  " + calculateTotalSalary(averageSalary, calculatePersonalIncomeTax(averageSalary), calculateSocialSecurityTax(averageSalary)));
+        System.out.println();
+        System.out.println("Task 14.");
+        boolean[] array2 = {true, true, true, false};
+        boolean[] array3 = {false, false, false, true};
+        System.out.println("The results of the XOR are: " + printXOR(array2) + " and " + printXOR(array3));
+        System.out.println();
+        System.out.println("Task 15.");
+        int[] array4 = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+        System.out.println("The negative numbers of the array are: " + Arrays.toString(printNegativeNumbers(array4)));
     }
+
     //Task 1
     public static void printName(String name) {
         System.out.println("Hello, my name is: " + name);
 
     }
+
     //Task 2
     public static void printMessage() {
         System.out.println("Welcome to the program!");
     }
+
     //Task 3
     public static double printThirtyPercent(int num) {
         return num * 0.3;
     }
+
     //Task 4
     public static String writeEmptyString(String[] array1) {
-        String nothing = " ";
-        return nothing;
+        return " ";
     }
+
     //Task 5
-    public static double get428Percent(double num) {
+    public static double get42Point8Percent(double num) {
         return num * 0.428;
     }
+
     //Task 6
     public static double writeDoubleLength(String str) {
         return str.length() * 2;
     }
+
     //Task 7
     public static int writeDifference(int num1, int num2) {
         return num2 - num1;
     }
+
     //Task 8
     public static double returnSquareRoot(String str) {
         int length = str.length();
         double sqrt = Math.sqrt(length);
         return sqrt;
     }
+
     //Task 9
     public static int returnFactorialLength(String str) {
         int length = str.length();
@@ -92,10 +119,70 @@ public class MethodsPartTwo {
         }
         return result;
     }
+
     //Task 10
     public static int printDividedValueBy100(int num) {
         return 100 / num;
     }
+
     //Task 11
-    
+    public static int printWeirdPrime(int num) {
+        if (primeNumberGenerator(num)) {
+            return num * num;
+        } else if (num % 4 == 0) {
+            return num * 2;
+        }
+        return num * 3;
+    }
+
+    public static boolean primeNumberGenerator(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    //Task 12
+    public static int[] printStringLengths(String[] str) {
+        int[] lengths = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            int length = str[i].length();
+            lengths[i] = length;
+        }
+        return lengths;
+    }
+
+    //Task 13
+    public static int calculatePersonalIncomeTax(int salary) {
+        return (int) (salary * 0.15);
+    }
+
+    public static int calculateSocialSecurityTax(int salary) {
+        return (int) (salary * 0.185);
+    }
+
+    public static int calculateTotalSalary(int salary, int personalTax, int socialTax) {
+        return salary - (personalTax + socialTax);
+    }
+
+    //Task 14
+    public static boolean printXOR(boolean[] a) {
+        boolean result = a[0];
+        for (int i = 1; i < a.length; i++) {
+            result = result ^ a[i];
+        }
+        return result;
+    }
+
+    //Task 15
+    public static int[] printNegativeNumbers(int[] numbers) {
+        int[] negativeNumbers = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                negativeNumbers[i] = numbers[i];
+            }
+        }
+        return negativeNumbers;
+    }
 }
---
+++ methodsparttwo/src/MethodsPartTwoTest.java
@@ -0,0 +1,133 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class MethodsPartTwoTest {
+    String[] testArray1 = {"nothing", "happens"};
+    String[] testArray2 = {"clutch", "brake", "gas"};
+    boolean[] testArray3 = {true, true, true, false};
+    boolean[] testArray4 = {false, false, false, true};
+    int[] testArray5 = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+    int[] testArray6 = {-5, -4, -3, -2, -1, 0, 0, 0, 0, 0, 0};
+
+    @Test
+        //Task 3
+    void shouldReturn30Percent() {
+        Assertions.assertEquals(30, MethodsPartTwo.printThirtyPercent(100));
+    }
+
+    @Test
+        //Task 4
+    void shouldReturnNothing() {
+        Assertions.assertArrayEquals(" ".toCharArray(), MethodsPartTwo.writeEmptyString(testArray1).toCharArray());
+    }
+
+    @Test
+        //Task 5
+    void shouldReturn42Point8Percent() {
+        Assertions.assertEquals(42.8, MethodsPartTwo.get42Point8Percent(100));
+    }
+
+    @Test
+        //Task 6
+    void shouldReturnDoubleLength() {
+        Assertions.assertEquals(44, MethodsPartTwo.writeDoubleLength("I will, but not today."));
+    }
+
+    @Test
+        //Task 7
+    void shouldReturnNoDifference() {
+        Assertions.assertEquals(0, MethodsPartTwo.writeDifference(11, 11));
+    }
+
+    @Test
+        //Task 7
+    void shouldReturnGreaterDifference() {
+        Assertions.assertEquals(1, MethodsPartTwo.writeDifference(10, 11));
+    }
+
+    @Test
+        //Task 7
+    void shouldReturnLesserDifference() {
+        Assertions.assertEquals(-1, MethodsPartTwo.writeDifference(11, 10));
+    }
+
+    @Test
+        //Task 8
+    void shouldReturnSquareRootLength() {
+        Assertions.assertEquals(4.69041575982343, MethodsPartTwo.returnSquareRoot("I will, but not today."));
+    }
+
+    @Test
+        //Task 9
+    void shouldReturnFactorialLength() {
+        Assertions.assertEquals(1, MethodsPartTwo.returnFactorialLength("I will, but not today."));
+    }
+
+    @Test
+        //Task 10
+    void shouldReturnNumber() {
+        Assertions.assertEquals(100, MethodsPartTwo.printDividedValueBy100(1));
+    }
+
+    @Test
+        //Task 11
+    void shouldReturnSquareBecauseNumberIsPrime() {
+        Assertions.assertEquals(121, MethodsPartTwo.printWeirdPrime(11));
+    }
+
+    @Test
+        //Task11
+    void shouldReturnNumberBecauseItIsDivisibleBy4AndNotPrime() {
+        Assertions.assertEquals(24, MethodsPartTwo.printWeirdPrime(12));
+    }
+
+    @Test
+        //Task 11
+    void shouldReturnNumberBecauseItIsNotDivisibleBy4AndNotPrime() {
+        Assertions.assertEquals(27, MethodsPartTwo.printWeirdPrime(9));
+    }
+
+    @Test
+        //Task 12
+    void shouldReturnTheLengthOfTheGivenWords() {
+        Assertions.assertEquals(6, 5, 3, Arrays.toString(MethodsPartTwo.printStringLengths(testArray2)));
+    }
+
+    @Test
+        //Task 13
+    void shouldReturn15Percent() {
+        Assertions.assertEquals(15000, MethodsPartTwo.calculatePersonalIncomeTax(100000));
+    }
+
+    @Test
+        //Task 13
+    void shouldReturn18Point5Percent() {
+        Assertions.assertEquals(18500, MethodsPartTwo.calculateSocialSecurityTax(100000));
+    }
+
+    @Test
+        //Task 13
+    void shouldReturnTotalValue() {
+        Assertions.assertEquals(66500, MethodsPartTwo.calculateTotalSalary(100000, 15000, 18500));
+    }
+
+    @Test
+        //Task 14
+    void shouldReturnTrueFor3TrueStatement() {
+        Assertions.assertTrue(true, String.valueOf(MethodsPartTwo.printXOR(testArray3)));
+    }
+
+    @Test
+        //Task 14
+    void shouldReturnTrueFor3FalseStatement() {
+        Assertions.assertTrue(true, String.valueOf(MethodsPartTwo.printXOR(testArray4)));
+    }
+
+    @Test
+        //Task15
+    void shouldReturnNegativeNumbers() {
+        Assertions.assertArrayEquals(testArray6, MethodsPartTwo.printNegativeNumbers(testArray5));
+    }
+}
